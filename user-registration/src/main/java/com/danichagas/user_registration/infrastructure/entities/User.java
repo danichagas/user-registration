3f94556dbbc0a389492025e5dde8e7cb53ae4f53
danichagas <chagasdaniel788@gmail.com>
2026-02-07 10:03:28 -0300

    feat: create user entity

---
+++ user-registration/src/main/java/com/danichagas/user_registration/infrastructure/entities/User.java
@@ -0,0 +1,28 @@
+package com.danichagas.user_registration.infrastructure.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "name")
+    private String name;
+
+
+}
