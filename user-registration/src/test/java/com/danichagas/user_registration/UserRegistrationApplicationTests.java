7ada1979f4e7835c278e03ed6ea49e801081de5c
danichagas <chagasdaniel788@gmail.com>
2026-02-06 23:33:57 -0300

    Initial commit

---
+++ user-registration/src/test/java/com/danichagas/user_registration/UserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danichagas.user_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
