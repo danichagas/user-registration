64e75fe7344434a57eb08a95fb19983e6663576a
danichagas <chagasdaniel788@gmail.com>
2026-02-07 13:11:44 -0300

    feat: find user by id

--- user-registration/src/main/java/com/danichagas/user_registration/business/UserService.java
+++ user-registration/src/main/java/com/danichagas/user_registration/business/UserService.java
@@ -4,6 +4,8 @@ import com.danichagas.user_registration.infrastructure.entities.User;
 import com.danichagas.user_registration.infrastructure.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class UserService {
 
@@ -16,4 +18,12 @@ public class UserService {
     public void registerUser(User user) {
         repository.saveAndFlush(user);
     }
+
+    public User searchUserById(UUID id) {
+        return repository.findById(id).orElseThrow(
+                () -> new RuntimeException("ID not found")
+        );
+    }
+
+
 }
